a4be164d7c360af7ba8102e1b3dd410e4c8bfdaa
Nikhil Patel <68939870+naive-nikhil@users.noreply.github.com>
2025-03-24 12:29:27 +0530

    Delete app directory

--- app/credentials.py
+++
@@ -1,3 +0,0 @@
-app_id = "21d76b0b-85ba-419a-b9c2-8228aa0abb3d"
-tenant_id = "6bbba376-ea68-4c6e-b6e4-6fd826b92b03"
-client_secret = "FEF8Q~gbJhtfwcXHgHzZYWOmLwu2hGGNcquYpaoe"
--- app/run.py
+++
@@ -1,7 +0,0 @@
-from app import app
-import os
-
-
-if __name__ == "__main__":
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-    app.run(debug=True, host="localhost", port=8178)
